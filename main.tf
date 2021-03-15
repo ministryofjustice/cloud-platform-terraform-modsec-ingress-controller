333ff68e2e5e80aa1c06362c3bfb63835c389404
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-03-15 16:02:40 +0200

    upg helm prov (#6)

--- main.tf
+++ main.tf
@@ -2,16 +2,11 @@
 # Helm #
 ########
 
-data "helm_repository" "modsec_ingress_nginx" {
-  name = "ingress-nginx"
-  url  = "https://kubernetes.github.io/ingress-nginx"
-}
-
 resource "helm_release" "modsec_ingress_nginx" {
   name       = var.controller_name
   chart      = "ingress-nginx"
   namespace  = "ingress-controllers"
-  repository = data.helm_repository.modsec_ingress_nginx.metadata[0].name
+  repository = "https://kubernetes.github.io/ingress-nginx"
   version    = "3.6.0"
 
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
