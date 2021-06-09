58f04c254fa9b3328509762b82d6cf1d8b0069d9
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-06-09 14:27:57 +0300

    Update main.tf (#7)

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ resource "helm_release" "modsec_ingress_nginx" {
   chart      = "ingress-nginx"
   namespace  = "ingress-controllers"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  version    = "3.6.0"
+  version    = "3.33.0"
 
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     controller_name = var.controller_name
     default_cert    = var.default_cert
-    replica_count    = var.replica_count
+    replica_count   = var.replica_count
   })]
 
   depends_on = [
